5342bf9902af22bbe9d189f7ad52cf24b25740b0
NicolasChalee <nico.chaleega@gmail.com>
2020-02-24 13:51:33 -0500

    Parte 2 Desarrollo-1

--- src/model/data_structures/Comparendo.java
+++ src/model/data_structures/Comparendo.java
@@ -2,7 +2,7 @@ package model.data_structures;
 
 import java.util.Date;
 
-public class Comparendo {
+public class Comparendo implements Comparable<Comparendo> {
 	
 	public int OBJECTID;
 
@@ -23,6 +23,59 @@ public class Comparendo {
 	public double longitud;
 	
 	public double latitud;
+
+	public int getOBJECTID() {
+		return OBJECTID;
+	}
+
+	public Date getFECHA_HORA() {
+		return FECHA_HORA;
+	}
+
+	public String getMEDIO_DETE() {
+		return MEDIO_DETE;
+	}
+
+	public String getCLASE_VEHI() {
+		return CLASE_VEHI;
+	}
+
+	public String getTIPO_SERVI() {
+		return TIPO_SERVI;
+	}
+
+	public String getINFRACCION() {
+		return INFRACCION;
+	}
+
+	public String getDES_INFRAC() {
+		return DES_INFRAC;
+	}
+
+	public String getLOCALIDAD() {
+		return LOCALIDAD;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+
+	@Override
+	public int compareTo(Comparendo comparendo) {
+		// TODO Auto-generated method stub
+		
+		if(FECHA_HORA.equals(comparendo.getFECHA_HORA())) 
+		{
+			if (OBJECTID < comparendo.getOBJECTID()) return 1;
+			else return -1;
+		}
+		else return FECHA_HORA.compareTo(comparendo.getFECHA_HORA());
+	}
 	
 	
 	
